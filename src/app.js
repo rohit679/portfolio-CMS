f00b13aaaf7d5cf1858d4e09a956a044c0a3d188
Rohit <rohit.prasad@plaxonic.com>
2021-05-16 21:06:40 +0530

    :recycle:code updated

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ export const createAnApp = () => {
     app.use(express.json());
     app.use(bodyParser.urlencoded({extended : true}));
     app.use(cors({
-        origin : 'http://localhost:3000',
+        origin : 'https://rohit-prasad-portfolio.herokuapp.com',
         optionsSuccessStatus : 200,
         method : 'GET, PUT, DELETE, POST'
     }));
