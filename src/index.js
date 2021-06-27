c3c3cfcece392b4ffa82868e2ea7299e17150a3b
Rohit <rohit.prasad@plaxonic.com>
2021-06-27 21:13:13 +0530

    code updated

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import { createAnApp, finishApp } from './app';
 import { httpHandler } from './config/http-handler';
 import './config/connection';
 import contentModule from './modules/contents';
-import authModule from './modules/auth';
 import {contentServices} from './modules/contents/services';
 
 const PORT = process.env.PORT || 8000;
 
 const app = createAnApp();
 
-authModule.init(app);
-
 contentModule.init(app);
 
 app.get('/',httpHandler(async (req, res, next) => { 
     const data = await contentServices.servePage();
+    // console.log("data : ", data);
     res.render('index',data);
 }));
 
